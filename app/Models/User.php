5c5b39fce8efd494b8ec11ca857a9fa7a7a8e794
Victor <victor.peng@foxmail.com>
2017-11-02 21:09:11 +0800

    Finish user signup

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,31 +1,41 @@
 <?php
 
-namespace App\Models;
+    namespace App\Models;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+    use Illuminate\Foundation\Auth\User as Authenticatable;
+    use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
-{
-    use Notifiable;
 
-    protected $table = 'users';
+    class User extends Authenticatable {
+        use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+        protected $table = 'users';
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-}
+        /**
+         * The attributes that are mass assignable.
+         *
+         * @var array
+         */
+        protected $fillable = [
+            'name',
+            'email',
+            'password',
+        ];
+
+        /**
+         * The attributes that should be hidden for arrays.
+         *
+         * @var array
+         */
+        protected $hidden = [
+            'password',
+            'remember_token',
+        ];
+
+        //头像
+        public function gravatar( $size = '100' ) {
+            $hash = md5( strtolower( trim( $this->attributes[ 'email' ] ) ) );
+
+            return "http://www.gravatar.com/avatar/$hash?s=$size";
+        }
+    }
